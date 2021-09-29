b81ed3c1e22e5fcd2de625d67b973f1be20c4a9c
gihenetu <86663241+gihenetu@users.noreply.github.com>
2021-09-29 13:16:05 +0100

    Update study_definition.py
    put in placeholders to expand on study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -8,7 +8,7 @@ study = StudyDefinition(
         "incidence": 0.5,
     },
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+        "2019-02-01", "2021-09-29"
     ),
 
 age=patients.age_as_of(
@@ -19,3 +19,101 @@ age=patients.age_as_of(
         },
     ),
 )
+
+###### 
+admission_date <- 
+date <- from 14 may 
+ineq dimensions 
+    # age <- 18 plus all adults
+    # sex 
+    # eth 
+            #ETHNICITY IN 16 CATEGORIES
+    ethnicity_16=patients.with_these_clinical_events(
+        ethnicity_codes_16,
+        returning="category",
+        find_last_match_in_period=True,
+        include_date_of_match=True,
+        return_expectations={
+            "category": {
+                "ratios": {
+                    "1": 0.0625,
+                    "2": 0.0625,
+                    "3": 0.0625,
+                    "4": 0.0625,
+                    "5": 0.0625,
+                    "6": 0.0625,
+                    "7": 0.0625,
+                    "8": 0.0625,
+                    "9": 0.0625,
+                    "10": 0.0625,
+                    "11": 0.0625,
+                    "12": 0.0625,
+                    "13": 0.0625,
+                    "14": 0.0625,
+                    "15": 0.0625,
+                    "16": 0.0625,
+                }
+            },
+            "incidence": 0.75,
+        },
+    ),
+
+   
+    # ETHNICITY IN 6 CATEGORIES
+    ethnicity=patients.with_these_clinical_events(
+        ethnicity_codes,
+        returning="category",
+        find_last_match_in_period=True,
+        include_date_of_match=True,
+        return_expectations={
+            "category": {"ratios": {"1": 0.2, "2":0.2, "3":0.2, "4":0.2, "5": 0.2}},
+            "incidence": 0.75,
+        },
+    ),
+    # dep 
+vaccination <- yes vaccination is vaccinated 2 weeks before admission, to get which proportion were vaccinated 2 weeks before
+    # First COVID vaccination administration codes
+    covadm1_dat=patients.with_vaccination_record(
+        returning="date",
+        tpp={
+            "target_disease_matches": "SARS-2 CORONAVIRUS",
+        },
+        emis={
+            "procedure_codes": codelists.covadm1,
+        },
+        find_first_match_in_period=True,
+        on_or_before="index_date", ### admissions date
+        on_or_after="2021-05-14",
+        date_format="YYYY-MM-DD",
+    ),
+    # Second COVID vaccination administration codes
+    covadm2_dat=patients.with_vaccination_record(
+        returning="date",
+        tpp={
+            "target_disease_matches": "SARS-2 CORONAVIRUS",
+        },
+        emis={
+            "procedure_codes": codelists.covadm2,
+        },
+        find_last_match_in_period=True,
+        on_or_before="index_date",
+        on_or_after="covadm1_dat + 19 days",
+        date_format="YYYY-MM-DD",
+    ),
+## comorbidities 
+    # preg (compare by months)
+    # BMI
+    smok
+    diab
+    hypert
+    dementia
+    LD
+    
+    maybe some medications that suppress immunity
+    
+covid testing
+covid cases
+admission due to covid
+region
+local authority
+first dose by 14 may
